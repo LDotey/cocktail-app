de4decd938637e1437eedabbaf8a87bbf2bc9273
Leigh Dotey <leighdotey@gmail.com>
2024-06-29 12:34:19 -0400

    data fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [drinks, setDrinks] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/drinks")
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+  }, [])
+
+  const handleAddDrink = (newDrink) => {
+    setDrinks(drinks => [...drinks, newDrink])
+    console.log("Added Drink!")
+  }
+
   return (
     <div className="App">
-      Hello, world. 
+      <hr/>
+      A B C Drinkie-poos. 
+      <hr/>
+
     </div>
   );
 }
