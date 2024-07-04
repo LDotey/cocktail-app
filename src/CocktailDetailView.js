526524414a64b2703baa538d91bc9fe368b7db74
Leigh Dotey <leighdotey@gmail.com>
2024-07-04 15:52:27 -0400

    adjust data structure

--- src/CocktailDetailView.js
+++ src/CocktailDetailView.js
@@ -6,6 +6,12 @@ function CocktailDetailView ({drink, onClose, onAddToFavourites}) {
             <div className="detail-view-content">
                 <button onClick={onClose}> X </button>
                 <h3>{drink.Name}</h3>
+                <li>{drink.Ingredient1 }</li>
+                <li>{drink.Ingredient2 }</li>
+                <li>{drink.Ingredient3 }</li>
+                <li>{drink.Ingredient4 }</li>
+                <li>{drink.Ingredient5 }</li>
+                
                 <p>{drink.Instructions}</p>
                 <button onClick={onAddToFavourites}> 🥃 </button>
             </div>
